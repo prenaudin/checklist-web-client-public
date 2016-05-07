d9b81aa328a9795d263ab8492518bb13ea976f4a
Pierre Renaudin <pierre.renaudin.fr@gmail.com>
2016-05-07 12:55:36 +0200

    Proxy request to API endpoint

--- server.prod.js
+++ server.prod.js
@@ -1,5 +1,6 @@
 /*eslint-disable */
 var express = require('express');
+var proxy = require('express-http-proxy');
 var path = require('path');
 var compression = require('compression');
 
@@ -8,6 +9,20 @@ app.set('port', process.env.PORT || 5001);
 
 app.use(compression());
 app.use(express.static('public'));
+
+// Proxy API requests
+var API_ENDPOINT = 'https://checklist-web-api.herokuapp.com';
+app.use('/api', proxy(API_ENDPOINT, {
+  forwardPath: function(req, res) {
+    return '/api' + require('url').parse(req.url).path;
+  }
+}));
+app.use('/public', proxy(API_ENDPOINT, {
+  forwardPath: function(req, res) {
+    return '/public' + require('url').parse(req.url).path;
+  }
+}));
+
 app.get('*',  function(req, res) {
   return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
